9664252d4075950a1a18ced61deec0aa86bbe4bf
sheriff <2356874721@qq.com>
2021-07-11 18:27:44 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,7 +4,7 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base')
   // console.log(baseConfig)
   it ('entry', () => {
-    assert.equal(baseConfig.entry.index, '/Users/liwenjing/Documents/myproject/my-webpack/npm-project/test/smoke/template/src/index.js')
-    assert.equal(baseConfig.entry.search, '/Users/liwenjing/Documents/myproject/my-webpack/npm-project/test/smoke/template/src/search.js')
+    assert.equal(baseConfig.entry.index.indexOf('npm-project/test/smoke/template/src/index.js') > -1, true)
+    assert.equal(baseConfig.entry.search.indexOf('npm-project/test/smoke/template/src/search.js') > -1, true)
   })
 })
